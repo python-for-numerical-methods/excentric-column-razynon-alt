c96e8078eff50ef04149f87bde6ebdff8ea8dd4e
razynon-alt <razynon@gmail.com>
2026-06-07 21:33:43 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -10,4 +10,19 @@ def find_critical_load(L, E, A, r, c, e, sigma_allow):
 
     Return: העומס P בניוטון (float)
     """
-    # כתבו כאן את הקוד
+    import numpy as np
+from scipy import optimize
+
+def column_stress_error(P, L, E, A, r, c, e, sigma_allow):
+    # פרמטרים לדוגמה
+    #A, E, L, r, e, c = 5000, 200000, 3000, 50, 20, 100
+    
+    # נוסחת הסקנט
+    sec_term = 1 / np.cos((L/(2*r)) * np.sqrt(P/(E*A)))
+    sigma_max = (P/A) * (1 + (e*c/r**2) * sec_term)
+    
+    return sigma_max - sigma_allow
+
+def find_critical_load(L, E, A, r, c, e, sigma_allow):
+    P_critical = optimize.newton(lambda P: column_stress_error(P, L, E, A, r, c, e, sigma_allow), 500000)
+    return P_critical
